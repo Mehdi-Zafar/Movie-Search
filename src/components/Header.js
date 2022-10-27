69c2f8b1c597737319d07763ade4c9e6b7e9fbc4
Mehdi-Zafar <mehdizaffar0@gmail.com>
2022-10-27 21:02:15 +0500

    1st commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return ( 
+        <nav>
+            <h1>Movie land</h1>
+        </nav>
+     );
+}
+ 
+export default Header;
---
+++ src/components/MovieDisplay.js
@@ -0,0 +1,63 @@
+import { useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { Dna } from "react-loader-spinner"
+
+const MovieDisplay = () => {
+
+    const [movies,setMovies] = useState([])
+    const [search,setSearch] = useState('')
+    const [loading,setLoading] = useState(false)
+    const api_url = "http://www.omdbapi.com/?i=tt3896198&apikey=341697da"
+
+
+    const searchMovies = async (title)=>{
+        setLoading(true)
+        const response = await fetch(`${api_url}&s=${title}`)
+        const data = await response.json()
+        console.log(data.Search)
+        setLoading(false)
+        setMovies(data.Search)
+    } 
+
+    return ( 
+        <>
+        {/* <h3 className="caption">Search your favorite movie</h3> */}
+        <div className="searchbar">
+            <input type="text" placeholder="Enter Movie Name" onChange={(e)=>setSearch(e.target.value)}/>
+            <button title="Search" onClick={()=>searchMovies(search)}><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+        </div>
+        {loading ? 
+        <div className="loader">
+            <Dna
+            visible={true}
+            height="200"
+            width="200"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+            />
+        </div>
+        :
+        ""}
+        {movies ?
+            <div className="movie-container">{movies && movies.map((movie)=>{
+                return (
+                    <div className="single-movie">
+                        <img src={movie.Poster !== "N/A" ? movie.Poster:"images/img-not-available.jpg"} alt="" />
+                        <p className="movie-type">{movie.Type}</p>
+                        <div className="movie-title">{movie.Title}</div>
+                        <p className="imdb-link"><a target="_blank" href={`https://www.imdb.com/title/${movie.imdbID}`}>See more info</a></p>
+                        <p className="movie-year">{movie.Year}</p>
+                    </div>
+                    )
+            })}</div>
+            :
+            <h2 className="noMovies">No Movies Found!</h2>
+        }
+        
+        </>
+     );
+}
+ 
+export default MovieDisplay;
